1c680dfba39a224435dcf83d7f2fecbaa3555ce4
LizaZou <119621159+LizaZou@users.noreply.github.com>
2023-04-21 12:21:13 +0200

    Add files via upload

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,6 +1,18 @@
 package org.insa.graphs.algorithm.shortestpath;
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
+
+    // Retrouver le graph.
+    Graph graph = data.getGraph();
+
+    //structure pour obtenir le sommet de plus court chemin
+    //ne contient que le sommet d'origine au début
+    BinaryHeap <Node> tas = new BinaryHeap<Node>(null);
+
+    
 
     public DijkstraAlgorithm(ShortestPathData data) {
         super(data);
@@ -10,7 +22,27 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
         ShortestPathSolution solution = null;
-        // TODO:
+        //initialisation :
+        for(Node n : graph.getNodes()){
+            //associe chaque sommet à un label dont le coût min 
+            //est inconnu, dont le coût est infni
+            Label l = new Label(n, null);
+            //on range les labels dans un tableu de labels rangés selon leur id (voir classe Node)
+            Label [] labelArray = new Label[graph.size()];
+            labelArray[n.getId()]=l;
+        }
+        tas.insert(data.getOrigin());
+
+        //itérations
+            while (){
+                //sélectionner le sommet de coût le plus faible et marquer ce sommet comme visité
+
+                //actualiser les coûts des sommets adjacents non marqués
+            }
+        
+
+        //arrêt lorsque tous les sommets sont marqués et retourner la solution
+
         return solution;
     }
 
---
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -0,0 +1,53 @@
+package org.insa.graphs.algorithm.shortestpath;
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Arc;
+
+
+public class Label {
+
+    public Label(Node n, Arc a){
+        this.currentNode=n;
+        this.currentValueShortestPath=Double.MAX_VALUE;
+        this.previousArc=a;
+        this.minCostIsKnown=false;
+    }
+    //sommet associé à ce label
+    private Node currentNode;
+
+    //marque : vrai si coût min de ce sommet est définitivement connu
+    private boolean minCostIsKnown;
+
+    //coût réalisé
+    private double currentValueShortestPath;
+
+    //arc venant du prédécesseur qui a le coût le plus faible
+    private Arc previousArc;
+
+    public Node getNode(){
+        return this.currentNode;
+    }
+
+    public boolean getMinCostIsKnown(){
+        return this.minCostIsKnown;
+    }
+
+    //coût réalisé (valeur courante du plus court chemin depuis l'origine vers le sommet)
+    public double getCurrentValueShortestPath(){
+        return this.currentValueShortestPath;
+    }
+
+    public Arc getPreviousArc(){
+        return this.previousArc;
+    }
+
+    public Arc setPreviousArc(){
+        return this.previousArc; //TO DO
+    }
+
+    public double getCost(){
+        return this.currentValueShortestPath;
+    }
+
+}
+
+
